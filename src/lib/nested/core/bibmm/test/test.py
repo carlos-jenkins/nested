091663484a7b44583b020f51e565bbac5b14fa82
Carlos Miguel Jenkins Pérez <carlos@jenkins.co.cr>
2012-09-01 19:23:51 -0600

    Better sane defaults and test fixed.

--- src/lib/nested/core/bibmm/test/test.py
+++ src/lib/nested/core/bibmm/test/test.py
@@ -1,11 +1,14 @@
 import gtk
+from os.path import normpath, dirname, abspath, realpath, join
 from nested.core.bibmm.bibmm import BibMM
+
+WHERE_AM_I = normpath(dirname(abspath(realpath(__file__))))
 
 # Run standalone
 if __name__ == '__main__':
     try:
         bib = BibMM()
-        bib.load_bib('test/test.bib')
+        bib.load_bib(join(WHERE_AM_I, 'test.bib'))
         gtk.main()
     except KeyboardInterrupt:
         pass
